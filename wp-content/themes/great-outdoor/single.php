140295ff1da53998f718438470e149a6b9d4e7b5
Josh Mobley <josh@newkind.com>
2016-03-14 11:48:14 -0400

    tweaking blog single for style

--- wp-content/themes/great-outdoor/single.php
+++ wp-content/themes/great-outdoor/single.php
@@ -3,16 +3,17 @@
 <section class="main-content blog-single">
 
     <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
+        <?php echo get_the_post_thumbnail(); ?>
 
+
+        <div class="blog-content">
     	<div class="blog-header">
-            <?php echo get_the_post_thumbnail(); ?>
+
        		<div class="blog-header-text">
        			<h2><?php esc_html( the_title() ); ?></h2>
         		<time><?php esc_html( the_date() ); ?></time>
         	</div>
         </div>
-
-        <div class="blog-content">
         <?php esc_html( the_content() ); ?>
        	</div>
 
