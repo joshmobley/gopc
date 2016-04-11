7eb7a23804a02f153069292da66725ff6567a559
Josh Mobley <josh@newkind.com>
2016-04-11 15:31:13 -0400

    pushing change to remove hard coded junk from adventure

--- wp-content/themes/great-outdoor/page-adventure.php
+++ wp-content/themes/great-outdoor/page-adventure.php
@@ -1,27 +1,8 @@
-<?php get_header();
+<?php get_header(); ?>
 
-	echo '<div class="main-content">';
+<section class="main-content">
 
-	$feature = get_field('featured-story');
-    $postImage = get_the_post_thumbnail( $feature->ID);
-
-	echo '<div class="full-width promo bg-image no-margin-top page-header" id="post-">';
-	echo '<a href="' . esc_url( get_the_permalink( $feature->ID ) )  . '">';
-	if( $postImage == null ){
-        $postImage = '<img src="http://placehold.it/1500x1000" />';
-    }
-    echo $postImage;
-	echo '<div class="promo-content">';
-	echo '<h2>' . esc_html( get_the_title( $feature->ID ) ) . '</h2>';
-
-	echo '<p>' . esc_html( get_the_excerpt( $feature->ID ) ) . '</p>';
-	echo '<a class="button" href="' . esc_url( get_the_permalink( $feature->ID ) ) . '">' . 'Read More' . ' &rarr;</a>';
-	echo '</div><!--.promo-content-->';
-	echo '</a>';
-	echo '</div><!--.full-width.promo-->';
-?>
-
-<?php get_sidebar('adventure'); ?>
+    <?php include( get_template_directory() . 'includes/content-parser.php'); ?>
 
 <?php
 
@@ -55,5 +36,5 @@ while ( $adventure->have_posts() ) : $adventure->the_post(); ?>
 	</ul>
 
 
-</div><!--.main-content-->
+</section><!--.main-content-->
 <?php get_footer(); ?>
