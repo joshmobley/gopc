3e1d9c36466d25397b6a55c77b4ead620ebb9a58
Josh Mobley <josh@newkind.com>
2016-04-12 11:24:06 -0400

    adding updates to activities view

---
+++ wp-content/themes/great-outdoor/archive.php
@@ -0,0 +1,72 @@
+<?php get_header(); ?>
+
+<section class="main-content">
+
+    <?php //include( get_template_directory() . '/includes/content-parser.php'); ?>
+
+    <h2 class="section-header is-page-title">
+        <?php the_archive_title(); ?>
+    </h2>
+
+
+
+<div class="archive-view">
+
+<?php
+
+    echo '<div class="post-filter">';
+
+        wp_list_categories();
+
+        wp_list_categories(
+            array(
+                'title_li' => 'Regions',
+                'taxonomy' => 'regions',
+            )
+        );
+
+        wp_list_categories(
+            array(
+                'title_li' => 'Activities',
+                'taxonomy' => 'activities',
+            )
+        );
+    echo '</div>';
+
+
+?>
+
+
+    <ul class="tiled-list">
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+        <?php
+        $postImage = get_the_post_thumbnail();
+        if( $postImage == null ){
+            $postImage = '<img src="http://placehold.it/300x250" />';
+        }
+        ?>
+
+        <li class="tile">
+            <a href="<?php esc_url( the_permalink() ); ?>"><?php echo $postImage; ?></a>
+            <a href="<?php esc_url( the_permalink() ); ?>"><h3><?php esc_html( the_title() ); ?></h3></a>
+            <?php
+                $body = get_the_excerpt();
+                echo esc_html(substr( $body, 0, strpos( $body, ' ', 85 ))) . '...';
+            ?>
+        </li>
+<?php endwhile; ?>
+
+        <div class="pagination-link"><?php posts_nav_link(); ?></div>
+
+<?php else : ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    </ul>
+<?php endif; ?>
+
+</div><!--.archive-view-->
+
+
+</section><!--.main-content-->
+<?php get_footer(); ?>
--- wp-content/themes/great-outdoor/page-adventure.php
+++ wp-content/themes/great-outdoor/page-adventure.php
@@ -4,18 +4,47 @@
 
     <?php include( get_template_directory() . '/includes/content-parser.php'); ?>
 
+
+<div class="archive-view">
+
 <?php
 
+    echo '<div class="post-filter">';
+
+        wp_list_categories();
+
+        wp_list_categories(
+            array(
+                'title_li' => 'Regions',
+                'taxonomy' => 'regions',
+            )
+        );
+
+        wp_list_categories(
+            array(
+                'title_li' => 'Activities',
+                'taxonomy' => 'activities',
+            )
+        );
+    echo '</div>';
+
+$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+
 $adventureArgs = array(
-	'post_type' => 'post'
+	'post_type' => 'post',
+    'paged' => $paged,
 );
+
+
 
 $adventure = new WP_Query( $adventureArgs );
 
 
+
 if ( $adventure->have_posts() ) :
 	echo '<ul class="tiled-list">';
-while ( $adventure->have_posts() ) : $adventure->the_post(); ?>
+
+    while ( $adventure->have_posts() ) : $adventure->the_post(); ?>
 
         <?php
         $postImage = get_the_post_thumbnail();
@@ -27,13 +56,27 @@ while ( $adventure->have_posts() ) : $adventure->the_post(); ?>
 		<li class="tile">
 			<a href="<?php esc_url( the_permalink() ); ?>"><?php echo $postImage; ?></a>
 			<a href="<?php esc_url( the_permalink() ); ?>"><h3><?php esc_html( the_title() ); ?></h3></a>
-			<?php esc_html(the_excerpt()); ?>
+			<?php
+                $body = get_the_excerpt();
+                echo esc_html(substr( $body, 0, strpos( $body, ' ', 85 ))) . '...';
+            ?>
 		</li>
 
-<?php endwhile; else : ?>
+
+
+<?php endwhile; ?>
+    <div class="pagination-link">
+        <?php previous_posts_link( 'Newer Posts', $adventure->max_num_pages ); ?>
+        <?php next_posts_link( 'Older Posts', $adventure->max_num_pages ); ?>
+    </div>
+    </ul>
+    <?php wp_reset_postdata(); ?>
+<?php else : ?>
 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
 <?php endif; ?>
-	</ul>
+
+</div><!--.archive-view-->
 
 
 </section><!--.main-content-->
