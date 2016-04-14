62ed3469087e15f682c5c2b352439bcfe33c8d95
Josh Mobley <josh@newkind.com>
2016-04-14 09:53:37 -0400

    adding brands to brand bar

--- wp-content/themes/great-outdoor/includes/brand-bar.php
+++ wp-content/themes/great-outdoor/includes/brand-bar.php
@@ -1,8 +1,31 @@
 <section class="page-section brand-bar">
 
-    <ul>
-        <li>Brand 1</li>
-        <li>Brand 2</li>
-    </ul>
+    <?php
+
+        $brandParent = get_page_by_title('brands')->ID;
+
+
+        $brands_args = array(
+            'post_type' => 'page',
+            'post_parent' => $brandParent,
+        );
+
+        // The Query
+        $brands = new WP_Query( $brands_args );
+
+        // The Loop
+        if ( $brands->have_posts() ) {
+            echo '<ul>';
+            while ( $brands->have_posts() ) {
+                $brands->the_post();
+                echo '<li>' . gopc_image( get_field(brand_logo) ) . '</li>';
+            }
+            echo '</ul>';
+        } else {
+            echo '<p> No posts. </p>';
+        }
+        wp_reset_postdata();
+
+    ?>
 
 </section><!--.brand-bar-->
