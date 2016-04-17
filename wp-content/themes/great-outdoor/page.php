f8e1b5231c9312d92db11478852df60a7ee3ce85
Josh Mobley <josh@newkind.com>
2016-04-17 17:21:14 -0400

    adding 'the-content' to pages

--- wp-content/themes/great-outdoor/page.php
+++ wp-content/themes/great-outdoor/page.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 	<section class="main-content">
+    <?php the_content(); ?>
 	<?php include( get_template_directory() . '/includes/content-parser.php'); ?>
 	</section>
 <?php get_footer(); ?>
