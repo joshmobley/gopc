e8649be9e0ec0cae234f4a6f0206b6fc3c1aa279
Josh Mobley <josh@newkind.com>
2016-04-17 17:04:28 -0400

    adding styles for search

--- wp-content/themes/great-outdoor/loop.php
+++ wp-content/themes/great-outdoor/loop.php
@@ -10,22 +10,31 @@
 
         <?php if( $posttype == 'locations' ): ?>
 
-        <h2><a href="<?php the_permalink(); ?>">Locations: <?php the_title(); ?></a></h2>
+            <h2><a href="<?php the_permalink(); ?>"><span class="type">Location </span>The <?php the_title(); ?> Shop</a></h2>
 
-        <div class="location-info">
-            <p class="address"><?php the_field('address'); ?></p>
-            <p class="telephone"><?php the_field('phone_number'); ?></p>
-        </div>
+            <div class="location-info">
+                <p class="address"><?php the_field('address'); ?></p>
+                <p class="telephone"><?php the_field('phone_number'); ?></p>
+            </div>
 
-        <div class="location-excerpt"><?php echo substr( get_field('location_description'), 0, 340 ) . '...';  ?></div>
+            <div class="location-excerpt"><?php echo substr( get_field('location_description'), 0, 340 ) . '...';  ?></div>
+
+        <?php elseif( $posttype == 'product' ): ?>
+
+            <h2><a href="<?php the_permalink(); ?>"><span class="type">Product </span><?php the_title(); ?></a></h2>
+
+            <time><?php echo get_the_date(); ?></time>
+
+            <p><?php the_excerpt(); ?></p>
 
         <?php else: ?>
 
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-        <time><?php echo get_the_date(); ?></time>
+            <h2><a href="<?php the_permalink(); ?>"><span class="type">Blog </span><?php the_title(); ?></a></h2>
 
-        <p><?php the_excerpt(); ?></p>
+            <time><?php echo get_the_date(); ?></time>
+
+            <p><?php the_excerpt(); ?></p>
 
         <?php endif; ?>
 
