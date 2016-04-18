a5b35397b5d57e48cf126c231ca8e463331125ab
Josh Mobley <josh@newkind.com>
2016-04-17 22:13:57 -0400

    blog single styles

--- wp-content/themes/great-outdoor/single.php
+++ wp-content/themes/great-outdoor/single.php
@@ -1,17 +1,24 @@
 <?php get_header(); ?>
 
-<section class="main-content blog-single">
+<section class="main-content">
 
     <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>
-        <?php echo get_the_post_thumbnail(); ?>
 
+        <?php $photoID = get_post_thumbnail_id($post->ID); ?>
+
+        <?php gopc_bgimageID( $photoID, '#bg-0' ); ?>
+
+
+
+        <div class="full-width promo no-margin-top page-header bg-image" id="post-0" style="height:250px;">
+           <div id="bg-0" class="promo-bg-image" style="height: 250px;"></div>
+        </div>
 
         <div class="blog-content">
     	<div class="blog-header">
 
        		<div class="blog-header-text">
        			<h2><?php esc_html( the_title() ); ?></h2>
-        		<time><?php esc_html( the_date() ); ?></time>
         	</div>
         </div>
         <?php esc_html( the_content() ); ?>
