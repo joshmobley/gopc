60c9e6a148e1c904fde20a0bc5970d9ebc3337fd
Josh Mobley <josh@newkind.com>
2016-04-18 13:48:18 -0400

    replacing hard-coded mega links with bloginfo(url)

--- wp-content/themes/great-outdoor/includes/mega-menu-products.php
+++ wp-content/themes/great-outdoor/includes/mega-menu-products.php
@@ -3,60 +3,60 @@
     <h3 class="mega-menu-subtitle">Product info, staff recommendations, picture and more.</h3>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/mens-clothing-2/">Men's Clothing</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/mens-clothing-2/">Men's Clothing</a></h4>
         <ul>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/mens-clothing/mens-shirts/">Shirts</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/mens-clothing/mens-shorts/">Shorts</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/mens-clothing/mens-swim/">Swim</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/mens-clothing/mens-pants/">Pants</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/mens-clothing/">All Mens</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/mens-clothing/mens-shirts/">Shirts</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/mens-clothing/mens-shorts/">Shorts</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/mens-clothing/mens-swim/">Swim</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/mens-clothing/mens-pants/">Pants</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/mens-clothing/">All Mens</a></li>
         </ul>
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/womens-clothing/">Women's Clothing</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/womens-clothing/">Women's Clothing</a></h4>
         <ul>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/women-clothing/womens-tops/">Tops</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/women-clothing/women-dresses/">Dresses &amp; Skirts</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/women-clothing/womens-shorts/">Shorts</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/women-clothing/womens-swim/">Swim</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/women-clothing/">All Womens</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/women-clothing/womens-tops/">Tops</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/women-clothing/women-dresses/">Dresses &amp; Skirts</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/women-clothing/womens-shorts/">Shorts</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/women-clothing/womens-swim/">Swim</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/women-clothing/">All Womens</a></li>
         </ul>
         </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/footwear/">Footwear</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/footwear/">Footwear</a></h4>
         <ul>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/footwear/mens-footwear/">Mens</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/footwear/womens-footwear/">Womens</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/footwear/kids-footwear/">Kids</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/footwear/">All Footwear</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/footwear/mens-footwear/">Mens</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/footwear/womens-footwear/">Womens</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/footwear/kids-footwear/">Kids</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/footwear/">All Footwear</a></li>
         </ul>
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/product-category/accessories/">Accessories</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/product-category/accessories/">Accessories</a></h4>
 
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/product-category/accessories/">Camp &amp; Hike Gear</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/product-category/accessories/">Camp &amp; Hike Gear</a></h4>
         <ul>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/camp-hike/tents/">Tents</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/camp-hike/packs/">Packs</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/camp-hike/sleeping/">Sleeping Systems</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/camp-hike/hydration/">Hydration</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/camp-hike/kitchen/">Camp Kitchen</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/camp-hike/">All Camp/Hike</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/camp-hike/tents/">Tents</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/camp-hike/packs/">Packs</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/camp-hike/sleeping/">Sleeping Systems</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/camp-hike/hydration/">Hydration</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/camp-hike/kitchen/">Camp Kitchen</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/camp-hike/">All Camp/Hike</a></li>
         </ul>
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/climb/">Climb</a></h4>
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/fish/">Fish</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/climb/">Climb</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/fish/">Fish</a></h4>
     </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/paddle/">Paddle</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/paddle/">Paddle</a></h4>
         <ul>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/paddle/kayaks/">Kayaks</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/paddle/canoes/">Canoes</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/paddle/paddle-boards/">Paddleboards</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/paddle/paddles/">Paddles</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/paddle/pdfs/">PFDs</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/product-category/paddle/">All Paddle</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/paddle/kayaks/">Kayaks</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/paddle/canoes/">Canoes</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/paddle/paddle-boards/">Paddleboards</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/paddle/paddles/">Paddles</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/paddle/pdfs/">PFDs</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/product-category/paddle/">All Paddle</a></li>
         </ul>
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/travel">Travel</a></h4>
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/pets/">Pets</a></h4>
-        <h4 class="mega-menu-column-title"><a href="https://greatoutdoor.wpengine.com/products/outdoor-fun/">Outdoor Fun</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/travel">Travel</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/pets/">Pets</a></h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/products/outdoor-fun/">Outdoor Fun</a></h4>
         <ul>
             <li><a href="#">Disc Golf</a></li>
             <li><a href="#">Kites</a></li>
@@ -68,11 +68,11 @@
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title">Featured Brands</h4>
         <ul>
-            <li><a href="#">Black Diamond</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/brands/osprey/">Osprey</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/brands/patagonia/">Patagonia</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/brands/smartwool/">Smartwool</a></li>
-            <li><a href="https://greatoutdoor.wpengine.com/brands/toad-co/">Toad &amp; Co</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/brands/black-diamond/">Black Diamond</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/brands/osprey/">Osprey</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/brands/patagonia/">Patagonia</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/brands/smartwool/">Smartwool</a></li>
+            <li><a href="<?php echo get_bloginfo('url'); ?>/brands/toad-co/">Toad &amp; Co</a></li>
         </ul>
     </div>
 
