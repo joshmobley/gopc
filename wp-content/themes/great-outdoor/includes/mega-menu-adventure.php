4ce33018cc924384f8c934b3ca227c5605b99ae2
Josh Mobley <josh@newkind.com>
2016-04-18 12:59:06 -0400

    adventure mega links

--- wp-content/themes/great-outdoor/includes/mega-menu-adventure.php
+++ wp-content/themes/great-outdoor/includes/mega-menu-adventure.php
@@ -3,55 +3,55 @@
     <h3 class="mega-menu-subtitle">Gear Tips, Expert Adveice, Adventure Stories, Club Connections &amp; more.</h3>
 
     <div class="mega-menu-column fifth">
-        <a class="activity-icon camp" href="#">Camp/Hike</a>
+        <a class="activity-icon camp" href="https://greatoutdoor.wpengine.com/activity/camp-hike-act/">Camp/Hike</a>
     </div>
 
     <div class="mega-menu-column fifth">
-        <a class="activity-icon climb" href="#">Climb</a>
+        <a class="activity-icon climb" href="https://greatoutdoor.wpengine.com/activity/climb-act/">Climb</a>
     </div>
 
     <div class="mega-menu-column fifth">
-        <a class="activity-icon fish" href="#">Fish</a>
+        <a class="activity-icon fish" href="https://greatoutdoor.wpengine.com/activity/fish-act/">Fish</a>
     </div>
 
     <div class="mega-menu-column fifth">
-        <a class="activity-icon paddle" href="#">Paddle</a>
+        <a class="activity-icon paddle" href="https://greatoutdoor.wpengine.com/activity/paddle-act/">Paddle</a>
     </div>
 
     <div class="mega-menu-column fifth">
-        <a class="activity-icon travel" href="#">Travel</a>
+        <a class="activity-icon travel" href="https://greatoutdoor.wpengine.com/activity/travel-act/">Travel</a>
     </div>
 
     <div class="mega-menu-column quarter">
-        <a href="#" class="color-box">
+        <a href="https://greatoutdoor.wpengine.com/category/destinations/" class="color-box">
             <h4>Trip Destinations</h4>
             <p>Local trips, tips &amp; guides.</p>
         </a>
     </div>
 
     <div class="mega-menu-column quarter">
-        <a href="#" class="color-box">
+        <a href="https://greatoutdoor.wpengine.com/category/stories-from-the-field/" class="color-box">
             <h4>Stories from the Field</h4>
             <p>From adventurers who were there</p>
         </a>
     </div>
 
     <div class="mega-menu-column quarter">
-        <a href="#" class="color-box">
+        <a href="https://greatoutdoor.wpengine.com/category/experts/" class="color-box">
             <h4>Expert Advice</h4>
             <p>Gear, How-to's, Readiness</p>
         </a>
     </div>
 
     <div class="mega-menu-column quarter">
-        <a href="#" class="color-box">
+        <a href="https://greatoutdoor.wpengine.com/category/company-culture/" class="color-box">
             <h4>Company Culture</h4>
             <p>What we're up to...</p>
         </a>
     </div>
 
     <div class="mega-menu-column full">
-        <a href="#" class="color-box">
+        <a href="https://greatoutdoor.wpengine.com/calendar/" class="color-box">
             <h4>Events Calendar</h4>
             <p>Join us on outdoor adventure or in-the-shop fun. Demos, Classes, Meetups, and more.</p>
         </a>
