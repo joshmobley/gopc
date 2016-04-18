6d01b03624bf306a98ecbbb1a8912c16b7e961dd
Josh Mobley <josh@newkind.com>
2016-04-18 11:53:20 -0400

    debugging

--- wp-content/themes/great-outdoor/includes/random-image-generator.php
+++ wp-content/themes/great-outdoor/includes/random-image-generator.php
@@ -16,21 +16,27 @@
 
             if( $activities[0]->slug == 'paddle-act'){
                 $postImage = wp_get_attachment_image_src( $paddleImg[$paddleCount], 'thumbnail' );
+                $postImage = '<img src="' . $postImage[0] . '" />';
                 $paddleCount++;
             }elseif( $activities[0]->slug == 'hiking-act'){
                 $postImage = wp_get_attachment_image_src( $campImg[$campCount], 'thumbnail' );
+                $postImage = '<img src="' . $postImage[0] . '" />';
                 $campCount++;
             }elseif( $activities[0]->slug == 'climb-act'){
                 $postImage = wp_get_attachment_image_src( $climbImg[$climbCount], 'thumbnail' );
+                $postImage = '<img src="' . $postImage[0] . '" />';
                 $climbCount++;
             }elseif( $activities[0]->slug == 'travel-act'){
                 $postImage = wp_get_attachment_image_src( $travelImg[$travelCount], 'thumbnail' );
+                $postImage = '<img src="' . $postImage[0] . '" />';
                 $travelCount++;
             }elseif( $activities[0]->slug == 'fish-act' ){
                 $postImage = wp_get_attachment_image_src( $fishImg[$fishCount], 'thumbnail' );
+                $postImage = '<img src="' . $postImage[0] . '" />';
                 $fishCount++;
             }else{
                 $postImage = wp_get_attachment_image_src( $generalImg[$generalCount], 'thumbnail' );
+                $postImage = '<img src="' . $postImage[0] . '" />';
                 $generalCount++;
             }
 
