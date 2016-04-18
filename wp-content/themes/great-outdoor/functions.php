17252050f51142c1ca786c0a57c989bfd3bf8d7c
Josh Mobley <josh@newkind.com>
2016-04-17 21:17:13 -0400

    adding random sort setting to woo

--- wp-content/themes/great-outdoor/functions.php
+++ wp-content/themes/great-outdoor/functions.php
@@ -91,6 +91,29 @@
     };
     add_filter('pre_get_posts', 'filter_search');
 
+    /* adding random sorting to woocommerce */
+
+    add_filter( 'woocommerce_get_catalog_ordering_args', 'custom_woocommerce_get_catalog_ordering_args' );
+
+    function custom_woocommerce_get_catalog_ordering_args( $args ) {
+        $orderby_value = isset( $_GET['orderby'] ) ? woocommerce_clean( $_GET['orderby'] ) : apply_filters( 'woocommerce_default_catalog_orderby', get_option( 'woocommerce_default_catalog_orderby' ) );
+
+        if ( 'random_list' == $orderby_value ) {
+            $args['orderby'] = 'rand';
+            $args['order'] = '';
+            $args['meta_key'] = '';
+        }
+        return $args;
+    }
+
+    add_filter( 'woocommerce_default_catalog_orderby_options', 'custom_woocommerce_catalog_orderby' );
+    add_filter( 'woocommerce_catalog_orderby', 'custom_woocommerce_catalog_orderby' );
+
+    function custom_woocommerce_catalog_orderby( $sortby ) {
+        $sortby['random_list'] = 'Random';
+        return $sortby;
+    }
+
 
 
     /*****************************************************************************
