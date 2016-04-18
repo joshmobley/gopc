159aeeb685d94e9c41143d0187e3b62f9960c3b9
Josh Mobley <josh@newkind.com>
2016-04-18 13:51:23 -0400

    linking up locations mega

--- wp-content/themes/great-outdoor/includes/mega-menu-locations.php
+++ wp-content/themes/great-outdoor/includes/mega-menu-locations.php
@@ -3,42 +3,42 @@
     <h3 class="mega-menu-subtitle">What's Happening in Your Local Shop?</h3>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Chapel Hill, NC</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/chapel-hill">Chapel Hill, NC</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             1800 East Franklin St<br/>
             919-933-6148</p>
     </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Charlotte, NC</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/charlotte">Charlotte, NC</a></h4>
         <p>Park Rd Shopping Ctr<br/>
             4341 Park Road<br/>
             704-523-1089</p>
     </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Charlottesville, VA</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/charlottesville">Charlottesville, VA</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             4341 Park Road<br/>
             704-523-1089</p>
     </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Greensboro, NC</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/greensboro">Greensboro, NC</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             4341 Park Road<br/>
             704-523-1089</p>
     </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Greenville, NC</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/greenville">Greenville, NC</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             4341 Park Road<br/>
             704-523-1089</p>
     </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Raleigh, NC</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/raleigh">Raleigh, NC</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             4341 Park Road<br/>
             704-523-1089</p>
@@ -47,21 +47,21 @@
 
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Virginia Beach, VA</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/virginia-beach">Virginia Beach, VA</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             4341 Park Road<br/>
             704-523-1089</p>
     </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Wilmington, NC</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/wilmington">Wilmington, NC</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             4341 Park Road<br/>
             704-523-1089</p>
     </div>
 
     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title">Winston Salem, NC</h4>
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/winston-salem">Winston Salem, NC</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             4341 Park Road<br/>
             704-523-1089</p>
