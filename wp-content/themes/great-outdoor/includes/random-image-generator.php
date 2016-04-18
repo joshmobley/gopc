873a944e12b95a9ceb947ae8d583e17643d309fc
Josh Mobley <josh@newkind.com>
2016-04-18 11:54:31 -0400

    fixing random thumbnails!

--- wp-content/themes/great-outdoor/includes/random-image-generator.php
+++ wp-content/themes/great-outdoor/includes/random-image-generator.php
@@ -5,42 +5,33 @@
 
         if( $postImage == null ){
 
-            print_r('randomizer');
-
-            print_r($post->ID);
-
             $activityObj = wp_get_post_terms( $post->ID, 'activities' );
 
-           // $activityObj = wp_get_post_terms( $post->ID, 'activities' );
-           // print_r( $activityObj);
-
             if( $activities[0]->slug == 'paddle-act'){
-                $postImage = wp_get_attachment_image_src( $paddleImg[$paddleCount], 'thumbnail' );
+                $postImage = wp_get_attachment_image_src( $paddleImg[$paddleCount], 'medium' );
                 $postImage = '<img src="' . $postImage[0] . '" />';
                 $paddleCount++;
             }elseif( $activities[0]->slug == 'hiking-act'){
-                $postImage = wp_get_attachment_image_src( $campImg[$campCount], 'thumbnail' );
+                $postImage = wp_get_attachment_image_src( $campImg[$campCount], 'medium' );
                 $postImage = '<img src="' . $postImage[0] . '" />';
                 $campCount++;
             }elseif( $activities[0]->slug == 'climb-act'){
-                $postImage = wp_get_attachment_image_src( $climbImg[$climbCount], 'thumbnail' );
+                $postImage = wp_get_attachment_image_src( $climbImg[$climbCount], 'medium' );
                 $postImage = '<img src="' . $postImage[0] . '" />';
                 $climbCount++;
             }elseif( $activities[0]->slug == 'travel-act'){
-                $postImage = wp_get_attachment_image_src( $travelImg[$travelCount], 'thumbnail' );
+                $postImage = wp_get_attachment_image_src( $travelImg[$travelCount], 'medium' );
                 $postImage = '<img src="' . $postImage[0] . '" />';
                 $travelCount++;
             }elseif( $activities[0]->slug == 'fish-act' ){
-                $postImage = wp_get_attachment_image_src( $fishImg[$fishCount], 'thumbnail' );
+                $postImage = wp_get_attachment_image_src( $fishImg[$fishCount], 'medium' );
                 $postImage = '<img src="' . $postImage[0] . '" />';
                 $fishCount++;
             }else{
-                $postImage = wp_get_attachment_image_src( $generalImg[$generalCount], 'thumbnail' );
+                $postImage = wp_get_attachment_image_src( $generalImg[$generalCount], 'medium' );
                 $postImage = '<img src="' . $postImage[0] . '" />';
                 $generalCount++;
             }
-
-          print_r($postImage);
 
         }
         ?>
