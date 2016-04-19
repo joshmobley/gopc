ea0964ed577133f9a721315b0ca1ae27b11ee394
Josh Mobley <josh@newkind.com>
2016-04-19 09:09:42 -0400

    adding locations landing stopgap

---
+++ wp-content/themes/great-outdoor/archive-locations.php
@@ -0,0 +1,82 @@
+<?php get_header(); ?>
+
+<div class="main-content">
+
+<div class="mega-menu is-static" id="locations-mega">
+    <h2 class="mega-menu-title">Visit us throughout NC &amp; VA</h2>
+    <h3 class="mega-menu-subtitle">What's Happening in Your Local Shop?</h3>
+
+    <div>
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/chapel-hill">Chapel Hill, NC</a></h4>
+        <p>Eastgate Shopping Ctr<br/>
+            1800 East Franklin St<br/>
+            919-933-6148</p>
+    </div>
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/charlotte">Charlotte, NC</a></h4>
+        <p>Park Rd Shopping Ctr<br/>
+        4341 Park Road<br/>
+        704-523-1089</p>
+    </div>
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/charlottesville">Charlottesville, VA</a></h4>
+        <p>Barracks Road Shopping Center<br/>
+            1125 Emmet St N<br />
+            434-995-5669</p>
+    </div>
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/greensboro">Greensboro, NC</a></h4>
+        <p>Friendly Shopping Center<br/>
+            3104 Northland Ave<br/>
+            336-851-1331</p>
+    </div>
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/greenville">Greenville, NC</a></h4>
+        <p>La Promenade II <br/>
+            530-D SE Greenville Blvd.<br/>
+            252-321-1308</p>
+    </div>
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/raleigh">Raleigh, NC</a></h4>
+        <p>Cameron Village<br/>
+            2017 Cameron St.<br/>
+            919-833-1741</p>
+    </div>
+
+
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/virginia-beach">Virginia Beach, VA</a></h4>
+        <p>Hilltop East Shopping Center<br/>
+            1556 Laskin Rd<br/>
+            757-962-6618</p>
+    </div>
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/wilmington">Wilmington, NC</a></h4>
+        <p>Hanover Center<br/>
+            3501 Oleander Dr. (US 76)<br/>
+            910-343-1648</p>
+    </div>
+
+    <div class="mega-menu-column fifth">
+        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/winston-salem">Winston Salem, NC</a></h4>
+        <p>Thruway Shopping Center<br/>
+            402 South Stratford Rd.<br/>
+            336-727-0906</p>
+    </div>
+
+</div>
+
+ </div><!--.mega-menu#products-mega-->
+
+</div>
+
+<?php get_footer(); ?>
