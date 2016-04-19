17fa15b211d197c31bc59bf584dd9399a87a0ab2
Josh Mobley <josh@newkind.com>
2016-04-18 21:08:21 -0400

    hide all brands after 10

--- wp-content/themes/great-outdoor/woocommerce/archive-product.php
+++ wp-content/themes/great-outdoor/woocommerce/archive-product.php
@@ -57,12 +57,10 @@ get_header( 'shop' );?>
 				do_action( 'woocommerce_before_shop_loop' );
 			?>
 
-
-
-                <?php echo do_shortcode('[searchandfilter id="35122"]'); ?>
+            <?php echo do_shortcode('[searchandfilter id="35122"]'); ?>
 			<?php woocommerce_product_loop_start(); ?>
-				<?php woocommerce_product_subcategories(); ?>
 
+				<?php woocommerce_product_subcategories(); ?>
 
 				<?php include( 'includes/category-headers.php'); ?>
 
