0310661b23c868f40f519d471bb4a1a9114bfcc8
Josh Mobley <josh@newkind.com>
2016-04-29 09:02:12 -0400

    removing duplicated content from page template (it's also in content parser)

--- wp-content/themes/great-outdoor/page.php
+++ wp-content/themes/great-outdoor/page.php
@@ -1,6 +1,5 @@
 <?php get_header(); ?>
 	<section class="main-content">
-    <?php the_content(); ?>
 	<?php include( get_template_directory() . '/includes/content-parser.php'); ?>
 	</section>
 <?php get_footer(); ?>
