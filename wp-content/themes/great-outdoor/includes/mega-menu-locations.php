22d8af307ce232d16b15a05ceb6002eb1e51526b
Josh Mobley <josh@newkind.com>
2016-06-05 21:11:15 -0400

    adding click-to-call links to all location telephone numbers!

--- wp-content/themes/great-outdoor/includes/mega-menu-locations.php
+++ wp-content/themes/great-outdoor/includes/mega-menu-locations.php
@@ -8,65 +8,63 @@
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/chapel-hill">Chapel Hill, NC</a></h4>
         <p>Eastgate Shopping Ctr<br/>
             1800 East Franklin St<br/>
-            919-933-6148</p>
+            <a href="tel:919-933-6148">919-933-6148</a></p>
     </div>
 
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/charlotte">Charlotte, NC</a></h4>
         <p>Park Rd Shopping Ctr<br/>
         4275 Park Road<br/>
-        704-523-1089</p>
+        <a href="tel:704-523-1089">704-523-1089</a></p>
     </div>
 
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/charlottesville">Charlottesville, VA</a></h4>
         <p>Barracks Road Shopping Center<br/>
             1125 Emmet St N<br />
-            434-995-5669</p>
+            <a href="tel:434-995-5669">434-995-5669</a></p>
     </div>
 
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/greensboro">Greensboro, NC</a></h4>
         <p>Friendly Shopping Center<br/>
             3104 Northline Ave<br/>
-            336-851-1331</p>
+            <a href="tel:336-851-1331">336-851-1331</a></p>
     </div>
 
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/greenville">Greenville, NC</a></h4>
         <p>La Promenade II <br/>
             530-D SE Greenville Blvd.<br/>
-            252-321-1308</p>
+            <a href="tel:252-321-1308">252-321-1308</a></p>
     </div>
 
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/raleigh">Raleigh, NC</a></h4>
         <p>Cameron Village<br/>
             2017 Cameron St.<br/>
-            919-833-1741</p>
+            <a href="tel:919-833-1741">919-833-1741</a></p>
     </div>
-
-
 
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/virginia-beach">Virginia Beach, VA</a></h4>
         <p>Hilltop East Shopping Center<br/>
             1556 Laskin Rd<br/>
-            757-962-6618</p>
+            <a href="tel:757-962-6618">757-962-6618</a></p>
     </div>
 
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/wilmington">Wilmington, NC</a></h4>
         <p>Hanover Center<br/>
             3501 Oleander Dr. (US 76)<br/>
-            910-343-1648</p>
+            <a href="tel:910-343-1648">910-343-1648</a></p>
     </div>
 
     <div class="mega-menu-column fifth">
         <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/winston-salem">Winston Salem, NC</a></h4>
         <p>Thruway Shopping Center<br/>
             402 South Stratford Rd.<br/>
-            336-727-0906</p>
+            <a href="tel:336-727-0906">336-727-0906</a></p>
     </div>
 
 </div>
