60d68e7db95147dd3f47f9b080332728b425761e
Josh Mobley <josh@newkind.com>
2016-06-14 07:21:10 -0400

    pushing style update for h1 and h2

--- wp-content/themes/great-outdoor/header.php
+++ wp-content/themes/great-outdoor/header.php
@@ -3,7 +3,7 @@
     <head>
         <title><?php bloginfo('name'); ?></title>
         <?php wp_head(); ?>
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.min.css?5.25.16a" />
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.min.css?6.14.16" />
 
         <!--[if IE lt 9]>
         <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.min.js"></script>
