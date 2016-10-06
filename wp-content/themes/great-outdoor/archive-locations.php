0e3b7b623e43cb313e898aab9ca13441d6bbceb2
Josh Mobley <josh@newkind.com>
2016-10-06 11:32:07 -0400

    pushing query-powered locations page/menu

--- wp-content/themes/great-outdoor/archive-locations.php
+++ wp-content/themes/great-outdoor/archive-locations.php
@@ -8,71 +8,47 @@
 
     <div>
 
-     <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/chapel-hill">Chapel Hill, NC</a></h4>
-        <p>Eastgate Shopping Ctr<br/>
-            1800 East Franklin St<br/>
-            <a href="tel:919-933-6148">919-933-6148</a></p>
+    <?php
+
+        $locationsArgs = array(
+            'post_type' => 'locations',
+        );
+
+        $locations = new WP_Query( $locationsArgs );
+
+        if ( $locations->have_posts() ){
+
+            while ( $locations->have_posts() ){
+                
+                $locations->the_post();
+
+                $address = get_field('address');
+                $center  = get_field('shopping_center');
+                 
+                echo '<div class="mega-menu-column fifth">' .
+                        '<h4 class="mega-menu-column-title">' .
+                            '<a href="' . get_the_permalink() . '">' .
+                                get_the_title() . 
+                            '</a>' .
+                        '</h4>' .
+                        '<p>' . $center . '<br/>' .
+                            $address . '</a>' .
+                        '</p>' .
+                    '</div>';
+                 
+
+            }
+
+            
+
+        }else{
+            echo 'Sorry, no posts found.';
+        }
+        wp_reset_postdata();
+
+    ?>
+
     </div>
-
-    <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/charlotte">Charlotte, NC</a></h4>
-        <p>Park Rd Shopping Ctr<br/>
-        4275 Park Road<br/>
-        <a href="tel:704-523-1089">704-523-1089</a></p>
-    </div>
-
-    <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/charlottesville">Charlottesville, VA</a></h4>
-        <p>Barracks Road Shopping Center<br/>
-            1125 Emmet St N<br />
-            <a href="tel:434-995-5669">434-995-5669</a></p>
-    </div>
-
-    <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/greensboro">Greensboro, NC</a></h4>
-        <p>Friendly Shopping Center<br/>
-            3104 Northline Ave<br/>
-            <a href="tel:336-851-1331">336-851-1331</a></p>
-    </div>
-
-    <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/greenville">Greenville, NC</a></h4>
-        <p>La Promenade II <br/>
-            530-D SE Greenville Blvd.<br/>
-            <a href="tel:252-321-1308">252-321-1308</a></p>
-    </div>
-
-    <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/raleigh">Raleigh, NC</a></h4>
-        <p>Cameron Village<br/>
-            2017 Cameron St.<br/>
-            <a href="tel:919-833-1741">919-833-1741</a></p>
-    </div>
-
-    <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/virginia-beach">Virginia Beach, VA</a></h4>
-        <p>Hilltop East Shopping Center<br/>
-            1556 Laskin Rd<br/>
-            <a href="tel:757-962-6618">757-962-6618</a></p>
-    </div>
-
-    <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/wilmington">Wilmington, NC</a></h4>
-        <p>Hanover Center<br/>
-            3501 Oleander Dr. (US 76)<br/>
-            <a href="tel:910-343-1648">910-343-1648</a></p>
-    </div>
-
-    <div class="mega-menu-column fifth">
-        <h4 class="mega-menu-column-title"><a href="<?php echo get_bloginfo('url'); ?>/locations/winston-salem">Winston Salem, NC</a></h4>
-        <p>Thruway Shopping Center<br/>
-            402 South Stratford Rd.<br/>
-            <a href="tel:336-727-0906">336-727-0906</a></p>
-    </div>
-
-
-</div>
 
  </div><!--.mega-menu#products-mega-->
 
