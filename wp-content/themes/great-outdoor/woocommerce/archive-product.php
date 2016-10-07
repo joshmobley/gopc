535d5d3bb08a6ffee9a04fdb18f17bd90077a246
Josh Mobley <josh@newkind.com>
2016-10-07 11:53:44 -0400

    removing promo code to fix timeout issue

--- wp-content/themes/great-outdoor/woocommerce/archive-product.php
+++ wp-content/themes/great-outdoor/woocommerce/archive-product.php
@@ -67,7 +67,7 @@ get_header( 'shop' );?>
 				<?php include( 'includes/category-headers.php'); ?>
 
 				<?php
-					$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+					/*$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 					
 					function get_term_top_most_parent($term_id, $taxonomy){
 					    // start from the current term
@@ -87,7 +87,7 @@ get_header( 'shop' );?>
 					$total_widgets = wp_get_sidebars_widgets();
         			$sidebar_widgets = count($total_widgets[$sidebar_id]);
 					//print_r($sidebar_widgets);
-					//dynamic_sidebar( 'product-mens-2' ); 
+					//dynamic_sidebar( 'product-mens-2' ); */
 				?>
 
 				<div class="promos count-<?php echo $sidebar_widgets; ?>">
