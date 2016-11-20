d94d32a6718785db83f4bccf85d3cae6196acd20
Josh Mobley <josh@newkind.com>
2016-11-20 18:52:15 -0500

    cachebustin css

--- wp-content/themes/great-outdoor/header.php
+++ wp-content/themes/great-outdoor/header.php
@@ -3,7 +3,7 @@
     <head>
         <title><?php bloginfo('name'); ?></title>
         <?php wp_head(); ?>
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.min.css?11.14.2016?a" />
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.min.css?11.14.2016?b" />
 
         <!--[if IE lt 9]>
         <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.min.js"></script>
