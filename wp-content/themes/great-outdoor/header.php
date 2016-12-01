95a7e9f20c1b70da090b7e398e80e588d5284208
Josh Mobley <josh@newkind.com>
2016-12-01 12:40:30 -0500

    adding fb widget

--- wp-content/themes/great-outdoor/header.php
+++ wp-content/themes/great-outdoor/header.php
@@ -33,6 +33,17 @@
     ?>
 
     <body class="<?php echo $posttype; ?>">
+    
+        <!-- START FACEBOOK SCRIPT -->
+        <div id="fb-root"></div>
+        <script>(function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) return;
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=196323433730322";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+        <!-- END FACEBOOK SCRIPT -->
 
         <header class="header">
 
