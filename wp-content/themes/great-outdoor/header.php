337f43bcae85eac80057b2996c67933310ad74a1
Josh Mobley <josh@joshmobley.net>
2017-07-31 22:54:40 -0400

    breaking css cache

--- wp-content/themes/great-outdoor/header.php
+++ wp-content/themes/great-outdoor/header.php
@@ -3,7 +3,7 @@
     <head>
         <title><?php bloginfo('name'); ?></title>
         <?php wp_head(); ?>
-        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.min.css?11.14.2016?b" />
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.min.css?7.31.2017" />
 
         <!--[if IE lt 9]>
         <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.min.js"></script>
